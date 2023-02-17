e48c05375a78dece45d5feef92f3fc903577667f
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2023-02-17 23:21:05 +0300

    Fix

--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom'
 
+
 import './index.css';
 import 'bootstrap/js/dist/carousel';
 
-
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
     <Router basename={'/'}>
         <App />
     </Router>
-
 );
