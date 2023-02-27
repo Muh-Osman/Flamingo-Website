4e2b69218090b3e4e1a443eabfa60eb06080cb0d
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2023-02-27 20:02:46 +0300

    Fix

--- src/App.js
+++ src/App.js
@@ -1,48 +1,79 @@
-import { Route, Routes, Navigate } from 'react-router-dom'
-import { Home, Phones, Computers, Watches, Cameras, Gaming, Tablets, Tvs, Accessories, Rent, ProductDetails, Apple, Samsung, Sony, Canon, Dell, Nintendo, NoMatch } from "./Pages"
-import { Header, Navbar, Carousel, HowItWorks, NewSectionShelves, TopBrands, Footer, MobileNavbar, FloatSearchMobile } from "./Sections"
-import { MainSiteContainer, CarouselAndShelvesContainer, ShelvesBox } from "./Components"
+import { Route, Routes, Navigate } from "react-router-dom";
+import {
+  Home,
+  Phones,
+  Computers,
+  Watches,
+  Cameras,
+  Gaming,
+  Tablets,
+  Tvs,
+  Accessories,
+  Rent,
+  ProductDetails,
+  Apple,
+  Samsung,
+  Sony,
+  Canon,
+  Dell,
+  Nintendo,
+  NoMatch,
+} from "./Pages";
+import {
+  Header,
+  Navbar,
+  Carousel,
+  HowItWorks,
+  NewSectionShelves,
+  TopBrands,
+  Footer,
+  MobileNavbar,
+  FloatSearchMobile,
+} from "./Sections";
+import {
+  MainSiteContainer,
+  CarouselAndShelvesContainer,
+  ShelvesBox,
+} from "./Components";
 
 // Import Home Page Data
-import ForYouData from './Data/HomeData/ForYouData'
-import MobilePhoneData from './Data/HomeData/MobilePhonesData'
-import GamingData from './Data/HomeData/GamingData'
-import SmartWatchesData from './Data/HomeData/SmartWatchesData'
-import CamerasData from './Data/HomeData/CamerasData'
-import ComputersData from './Data/HomeData/ComputersData'
-import TabletsData from './Data/HomeData/TabletsData'
-import TvsData from './Data/HomeData/TvsData'
+import ForYouData from "./Data/HomeData/ForYouData";
+import MobilePhoneData from "./Data/HomeData/MobilePhonesData";
+import GamingData from "./Data/HomeData/GamingData";
+import SmartWatchesData from "./Data/HomeData/SmartWatchesData";
+import CamerasData from "./Data/HomeData/CamerasData";
+import ComputersData from "./Data/HomeData/ComputersData";
+import TabletsData from "./Data/HomeData/TabletsData";
+import TvsData from "./Data/HomeData/TvsData";
 
 // Import Phones Page Data
-import GooglePhonesData from './Data/PhonesData/GooglePhonesData'
-import SamsungPhonesData from './Data/PhonesData/SamsungPhonesData'
-import ApplePhonesData from './Data/PhonesData/ApplePhonesData'
+import GooglePhonesData from "./Data/PhonesData/GooglePhonesData";
+import SamsungPhonesData from "./Data/PhonesData/SamsungPhonesData";
+import ApplePhonesData from "./Data/PhonesData/ApplePhonesData";
 
 // Import Gaming Page Data
-import SonyGamingData from './Data/GamingData/SonyGamingData'
+import SonyGamingData from "./Data/GamingData/SonyGamingData";
 // Import Watches Page Data
-import AppleWatchesData from './Data/WatchesData/AppleWatchesData'
+import AppleWatchesData from "./Data/WatchesData/AppleWatchesData";
 // Import Cameras Page Data
-import CanonCamerasData from './Data/CamerasData/CanonCamerasData'
+import CanonCamerasData from "./Data/CamerasData/CanonCamerasData";
 // Import Computers Page Data
-import AppleComputersData from './Data/ComputersData/AppleComputersData'
+import AppleComputersData from "./Data/ComputersData/AppleComputersData";
 // Import Tablets Page Data
-import AppleTabletsData from './Data/TabletsData/AppleTabletsData'
+import AppleTabletsData from "./Data/TabletsData/AppleTabletsData";
 // Import TVs Page Data
-import LgTvsData from './Data/TabletsData/AppleTabletsData'
+import LgTvsData from "./Data/TvsData/LgTvsData";
 // Import Accessories Page Data
-import ToolsData from './Data/AccessoriesData/ToolsData'
-
+import ToolsData from "./Data/AccessoriesData/ToolsData";
 
 const App = () => {
-
   // Load my script
   var loadScript = function (src) {
-    var tag = document.createElement('script');
+    var tag = document.createElement("script");
     tag.async = true;
     tag.src = src;
-    document.getElementsByTagName('body')[0].appendChild(tag);
-  }
+    document.getElementsByTagName("body")[0].appendChild(tag);
+  };
 
   return (
     <>
@@ -53,43 +84,96 @@ const App = () => {
           <Carousel />
           <ShelvesBox>
             <Routes>
+              <Route path="/" element={<Navigate to="/home" />} />
+              <Route path="/home" element={<Home />} />
+              <Route
+                path="home/foryou/:productId"
+                element={<ProductDetails item={ForYouData} />}
+              />
+              <Route
+                path="home/mobilephones/:productId"
+                element={<ProductDetails item={MobilePhoneData} />}
+              />
+              <Route
+                path="home/gaming/:productId"
+                element={<ProductDetails item={GamingData} />}
+              />
+              <Route
+                path="home/smartwatches/:productId"
+                element={<ProductDetails item={SmartWatchesData} />}
+              />
+              <Route
+                path="home/cameras/:productId"
+                element={<ProductDetails item={CamerasData} />}
+              />
+              <Route
+                path="home/computers/:productId"
+                element={<ProductDetails item={ComputersData} />}
+              />
+              <Route
+                path="home/tablets/:productId"
+                element={<ProductDetails item={TabletsData} />}
+              />
+              <Route
+                path="home/tvs/:productId"
+                element={<ProductDetails item={TvsData} />}
+              />
 
-              <Route path='/' element={<Navigate to="/home" />} />
-              <Route path='/home' element={<Home />} />
-              <Route path='home/foryou/:productId' element={<ProductDetails item={ForYouData} />} />
-              <Route path='home/mobilephones/:productId' element={<ProductDetails item={MobilePhoneData} />} />
-              <Route path='home/gaming/:productId' element={<ProductDetails item={GamingData} />} />
-              <Route path='home/smartwatches/:productId' element={<ProductDetails item={SmartWatchesData} />} />
-              <Route path='home/cameras/:productId' element={<ProductDetails item={CamerasData} />} />
-              <Route path='home/computers/:productId' element={<ProductDetails item={ComputersData} />} />
-              <Route path='home/tablets/:productId' element={<ProductDetails item={TabletsData} />} />
-              <Route path='home/tvs/:productId' element={<ProductDetails item={TvsData} />} />
+              <Route path="/phones" element={<Phones />} />
+              <Route
+                path="phones/google/:productId"
+                element={<ProductDetails item={GooglePhonesData} />}
+              />
+              <Route
+                path="phones/samsung/:productId"
+                element={<ProductDetails item={SamsungPhonesData} />}
+              />
+              <Route
+                path="phones/apple/:productId"
+                element={<ProductDetails item={ApplePhonesData} />}
+              />
 
-              <Route path='/phones' element={<Phones />} />
-              <Route path='phones/google/:productId' element={<ProductDetails item={GooglePhonesData} />} />
-              <Route path='phones/samsung/:productId' element={<ProductDetails item={SamsungPhonesData} />} />
-              <Route path='phones/apple/:productId' element={<ProductDetails item={ApplePhonesData} />} />
+              <Route path="/gaming" element={<Gaming />} />
+              <Route
+                path="gaming/sony/:productId"
+                element={<ProductDetails item={SonyGamingData} />}
+              />
 
-              <Route path='/gaming' element={<Gaming />} />
-              <Route path='gaming/sony/:productId' element={<ProductDetails item={SonyGamingData} />} />
+              <Route path="/watches" element={<Watches />} />
+              <Route
+                path="watches/apple/:productId"
+                element={<ProductDetails item={AppleWatchesData} />}
+              />
 
-              <Route path='/watches' element={<Watches />} />
-              <Route path='watches/apple/:productId' element={<ProductDetails item={AppleWatchesData} />} />
+              <Route path="/cameras" element={<Cameras />} />
+              <Route
+                path="cameras/canon/:productId"
+                element={<ProductDetails item={CanonCamerasData} />}
+              />
 
-              <Route path='/cameras' element={<Cameras />} />
-              <Route path='cameras/canon/:productId' element={<ProductDetails item={CanonCamerasData} />} />
+              <Route path="/computers" element={<Computers />} />
+              <Route
+                path="computers/apple/:productId"
+                element={<ProductDetails item={AppleComputersData} />}
+              />
 
-              <Route path='/computers' element={<Computers />} />
-              <Route path='computers/apple/:productId' element={<ProductDetails item={AppleComputersData} />} />
+              <Route path="/tablets" element={<Tablets />} />
+              <Route
+                path="tablets/apple/:productId"
+                element={<ProductDetails item={AppleTabletsData} />}
+              />
 
-              <Route path='/tablets' element={<Tablets />} />
-              <Route path='tablets/apple/:productId' element={<ProductDetails item={AppleTabletsData} />} />
+              <Route path="/tvs" element={<Tvs />} />
+              <Route
+                path="tvs/lg/:productId"
+                element={<ProductDetails item={LgTvsData} />}
+              />
 
-              <Route path='/tvs' element={<Tvs />} />
-              <Route path='tvs/lg/:productId' element={<ProductDetails item={LgTvsData} />} />
-
-              <Route path='/accessories' element={<Accessories />} />
-              <Route path='accessories/tools/:productId' element={<ProductDetails item={ToolsData} />} />
+              <Route path="/accessories" element={<Accessories />} />
+              <Route
+                path="accessories/tools/:productId"
+                element={<ProductDetails item={ToolsData} />}
+              />
 
               <Route path="/rent" element={<Rent />} />
 
@@ -102,7 +186,6 @@ const App = () => {
 
               <Route path="*" element={<NoMatch />} />
             </Routes>
-
           </ShelvesBox>
         </CarouselAndShelvesContainer>
       </MainSiteContainer>
@@ -115,9 +198,9 @@ const App = () => {
       <MobileNavbar />
       <FloatSearchMobile />
 
-      {loadScript('main.js')}
+      {loadScript("main.js")}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Pages/Tvs/Tvs.js
+++ src/Pages/Tvs/Tvs.js
@@ -1,75 +1,57 @@
-import './Tvs.css'
-import { useState } from 'react'
-import LgTvsItemsData from '../../Data/WatchesData/AppleWatchesData'
-import { ShelveConainer, ItemShelves, Button } from '../../Components'
-
-
+import "./Tvs.css";
+import { useState } from "react";
+import LgTvsItemsData from "../../Data/TvsData/LgTvsData";
+import { ShelveConainer, ItemShelves, Button } from "../../Components";
 
 const Tvs = () => {
-
-
-
   // Hide Big more Button & add components
-  const [state, setState] = useState(false)
-
+  const [state, setState] = useState(false);
 
   // Loop on Data
-  const lgTvsItems = LgTvsItemsData.map(item => {
-    return <ItemShelves
-      className={'item-cards'}
-      key={item.id}
-      item={item}
-    />
-  })
-
+  const lgTvsItems = LgTvsItemsData.map((item) => {
+    return <ItemShelves className={"item-cards"} key={item.id} item={item} />;
+  });
 
   return (
-
     <>
-
-      <ShelveConainer href='https://www.google.com/' title='LG'>
+      <ShelveConainer href="https://www.google.com/" title="LG">
         {lgTvsItems}
       </ShelveConainer>
 
-
-
-
       {/* Hide Big more Button & add components */}
 
-      {
-        state ? (
-          <>
-            < ShelveConainer href='https://www.google.com/' title='Samsung'>
-              { }
-            </ShelveConainer>
+      {state ? (
+        <>
+          <ShelveConainer href="https://www.google.com/" title="Samsung">
+            {}
+          </ShelveConainer>
 
-            <ShelveConainer href='https://www.google.com/' title='Sony'>
-              { }
-            </ShelveConainer>
+          <ShelveConainer href="https://www.google.com/" title="Sony">
+            {}
+          </ShelveConainer>
 
-            <ShelveConainer href='https://www.google.com/' title='TCL'>
-              { }
-            </ShelveConainer>
+          <ShelveConainer href="https://www.google.com/" title="TCL">
+            {}
+          </ShelveConainer>
 
-            <ShelveConainer href='https://www.google.com/' title='Panasonic '>
-              { }
-            </ShelveConainer>
+          <ShelveConainer href="https://www.google.com/" title="Panasonic ">
+            {}
+          </ShelveConainer>
 
-            <ShelveConainer href='https://www.google.com/' title='Hisense'>
-              { }
-            </ShelveConainer>
-          </>
-
-        ) : (
-
-          //  <Big More Button 
-          <Button onClick={() => setState(true)} className={'big-more-btn'} title={'More'} />
-
-        )
-      }
-
+          <ShelveConainer href="https://www.google.com/" title="Hisense">
+            {}
+          </ShelveConainer>
+        </>
+      ) : (
+        //  <Big More Button
+        <Button
+          onClick={() => setState(true)}
+          className={"big-more-btn"}
+          title={"More"}
+        />
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Tvs
+export default Tvs;
