e5d623f84fe4d986e439eaabbafdf483cdde7574
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2023-05-27 20:39:55 +0300

    Fix

--- src/Pages/ProductDetails/ProductDetails.js
+++ src/Pages/ProductDetails/ProductDetails.js
@@ -11,8 +11,7 @@ const ProductDetails = () => {
 
   // Get URL then split it ex: (http://localhost:3000/phones/apple/2)
   let url = useLocation().pathname.split("/"); // ['...', 'phones', 'apple', '2']
-  let [, category, brand, id] = [...url]
-
+  let [, category, brand, id] = [...url];
 
   // fetch Data from Redux
   const dispatch = useDispatch();
@@ -25,15 +24,21 @@ const ProductDetails = () => {
     // Apply some CSS Style onmount <ProductDetails /> Component
     const webStyle = document.getElementById("root");
     webStyle.classList.add("style-onclick-item");
+
+    // Keep active style to home btn even after render <ProductDetails>
+    if (category === "home") {
+      webStyle.classList.add("active-home-btn");
+    }
+
     // CleanUp (remove class onUnmount)
     return () => {
       webStyle.classList.remove("style-onclick-item");
+      webStyle.classList.remove("active-home-btn");
     };
   }, []);
 
   //   Get Data from Redux
-  let data = useSelector(data => data.products[brand][id - 1]);
-
+  let data = useSelector((data) => data.products[brand][id - 1]);
 
   return (
     <>
